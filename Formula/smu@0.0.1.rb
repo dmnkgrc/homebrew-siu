ea5db01cffced50cd313271f2522b40794e5d29e
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2023-06-20 13:48:04 +0200

    initial commit

---
+++ Formula/smu@0.0.1.rb
@@ -0,0 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class SmuAT001 < Formula
+  desc ""
+  homepage ""
+  license "MIT"
+  url "https://github.com/dmnkgrc/smu.git", tag: "v0.0.1"
+  head "https://github.com/dmnkgrc/smu.git"
+
+  depends_on "rust" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test smu@0.0.1`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
