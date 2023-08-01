503c3b0cb78082b427d921d70cabd98d9c5ebb54
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2023-08-01 22:15:13 +0200

    chore: release new version

--- Formula/siu.rb
+++ Formula/siu.rb
@@ -1,7 +1,7 @@
 class Siu < Formula
   desc "CLI to help you and your team setup computers and projects fast"
   homepage "https://github.com/dmnkgrc/siu"
-  url "https://github.com/dmnkgrc/siu.git", tag: "v0.2.1"
+  url "https://github.com/dmnkgrc/siu.git", tag: "v0.2.2"
   license "MIT"
   head "https://github.com/dmnkgrc/siu.git", branch: "main"
 
